f705d3f02233b7cb9e3b0a5fc1070be8f9f7a40d
omar-fares13 <omarfares820214@gmail.com>
2025-07-10 09:31:18 +0300

    data base configration changes

--- src/db.js
+++ src/db.js
@@ -1,15 +1,24 @@
 import pkg from 'pg';
 const { Pool } = pkg;
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT, 
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false, //for Render
+        },
+      }
+    : {
+        user: process.env.USER,
+        host: process.env.HOST,
+        database: process.env.DATABASE,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+      }
+);
 
 export default pool;
